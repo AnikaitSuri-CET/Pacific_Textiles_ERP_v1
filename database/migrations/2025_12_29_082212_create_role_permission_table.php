4c91524a515487c7a78d1884a2a92f610ee85c23
AnikaitSuri-CET <anikait@codeedgetech.com>
2025-12-29 14:20:08 +0530

    updated role_permission table

--- database/migrations/2025_12_29_082212_create_role_permission_table.php
+++ database/migrations/2025_12_29_082212_create_role_permission_table.php
@@ -6,20 +6,32 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
         Schema::create('role_permission', function (Blueprint $table) {
-            $table->id();
+            $table->id();                                               // Auto Increment
+            
+            $table->foreignId('role_id')                                // References roles.id
+                  ->constrained('roles')
+                  ->onDelete('cascade');
+            
+            $table->foreignId('premission_id')                          // References permissions.id
+                  ->constrained('permissions')
+                  ->onDelete('cascade');
+            
+            $table->timestamp('granted_at')->useCurrent();              // When permission was granted
+
+            $table->foreignId('granted_by')                             // Admin user who granted
+                  ->nullable()
+                  ->constrained('users')
+                  ->nullOnDelete();
+            
             $table->timestamps();
+
+            $table->unique(['role_id', 'permission_id']);               // Unique constraint for roles.id and permissions.id
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('role_permission');
